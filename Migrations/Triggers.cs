63add919ba80b6269913f7438c35970d41415713
anna <tikto000000@gmail.com>
2024-11-06 14:09:07 +0300

    added trigger for db

---
+++ Migrations/Triggers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using System.Data.Entity.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class Triggers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE TRIGGER TRG_PreventDropDB ON ALL SERVER " +
+                "FOR DROP_DATABASE AS BEGIN " +
+                "    RAISERROR('You cannot drop databases on this server.', 16,1)" +
+                "    ROLLBACK; " +
+                "END");
+        }
+
+        public override void Down()
+        {
+            Sql("DROP TRIGGER TRG_PreventDropDB");
+        }
+    }
+}
